5ac2edf139914263a19de26b023c848e48f11a32
Gabriel NUñez V <38358137+Makotobka@users.noreply.github.com>
2021-09-06 16:36:06 -0500

    FRONT: Cambios en html

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
           Test de Pull Request
         </v-card-title>
         <v-card-text>
-          <div>Dentro del directorio <pre>/pages</pre> cree archivos <code>.vue</code> para que se muestren en el <b>Menú</b></div>
+          <div>Porque aqui paso alguien<pre>/pages</pre> cree archivos <code>.vue</code> para que se muestren en el <b>Menú</b></div>
           <hr>
           <div>
             Para imitar una migracion ejecuta <code>migracion nueva</code> dónde nueva es el nombre de la migracion
