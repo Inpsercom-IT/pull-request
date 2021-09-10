c953444c70ef36a6e8902c4c44e1291ed66ef599
Gabriel NUñez V <38358137+Makotobka@users.noreply.github.com>
2021-09-10 14:29:11 -0500

    solucion finalizada N° 20

---
+++ pages/gabriel.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Prueba de gabriel
+        </v-card-title>
+        <v-card-text>
+          <div>
+              item del menu
+          </div>
+          <hr>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
